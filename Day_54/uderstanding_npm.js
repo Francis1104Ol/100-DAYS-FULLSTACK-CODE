a403d4e0c3ae645d7bb6b8705f5d4d87a2e36073
Francis1104OL <taiwofrancis001@gmail.com>
2026-02-28 22:55:45 +0100

    Day_54

---
+++ Day_54/uderstanding_npm.js
@@ -0,0 +1,9 @@
+// What is NPM it stand for node package manager 
+//it is both a command line interface for managing packages and also repository to install packages 
+// different types of packages we can have in our application Regular dependencies and development dependeces
+
+// types of package install local and global package 
+//Architecture of node 
+//main thread :the heart of node js 
+//event loop
+//Thread pool 

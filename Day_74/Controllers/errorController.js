cccf601230fe103cbd4e250bb5bacf61fa235827
Francis1104OL <taiwofrancis001@gmail.com>
2026-03-20 23:20:17 +0100

    Day_74

---
+++ Day_74/Controllers/errorController.js
@@ -0,0 +1,34 @@
+const { stackTraceLimit } = require("../Utils/CustomError");
+
+const devErrors = (res, error)=>{
+    res.status(error.statusCode).json({
+        status:error.statusCode,
+        message:error.message,
+        stackTrace:error.stack,
+        error:error
+    });
+}
+const prodErrors= (res, error)=>{
+    if(error.isOperational){
+     res.status(error.statusCode).json({
+        status:error.statusCode,
+        message:error.message,
+     })
+    }else{
+        res.status(500).json({
+            status: 'error',
+            message:'Something went wrong! Please try again later.'
+        })
+    }
+}
+
+module.exports=(error, req, res, next)=>{
+    error.statusCode = error.statusCode || 500
+    error.status = error.status||'error';
+
+    if(process.env.NODE_ENV ==='development'){
+        devErrors(res, error)
+}else if(process.env.Node_ENV ==='production'){
+    prodErrors(res, error)
+}
+}

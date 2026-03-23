f2e00ba49f9b0a37471cf1b59097c5ed1f6d4d36
Francis1104OL <taiwofrancis001@gmail.com>
2026-03-23 18:31:18 +0100

    Day_77

---
+++ Day_77/Models/userModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const { validate } = require('./movieModel')
+const bcrypt = require('bcryptjs')
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        require:[true,'Please Enter your name']
+    },
+    email:{
+        type: String,
+        require: [true,'Please Enter your email'],
+        unique:true,
+        lowercase:true,
+        validate:[validator.isEmail, 'Please Enter a valid email']
+    },
+    photo:String,
+    password:{
+        type: String,
+        require:[true, ' Please Enter a password'],
+        minLength:8
+    },
+    confirmPassword:{
+        type:String,
+        require:[true, 'Please confirm your pass word'],
+        //this validator will only work for save() and create()
+        validate:{
+            validator: function(val){
+                return val ===this.password
+            },
+            message: 'Password & confirm Password does not match'
+        }
+    }
+})
+
+
+userSchema.pre('save', async function(next){
+    //this check is the password has not been modified
+    if(!this.isModified('password')) return ;
+
+    // if it has not been modified then we encrypt async we also have sync version of it
+   this.password = await bcrypt.hash(this.password, 12)
+
+   this.confirmPassword = undefined
+   
+})
+const User = mongoose.model('User', userSchema);
+module.exports = User

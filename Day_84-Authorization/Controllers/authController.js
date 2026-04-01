0b4a4112d4f157a933fdc52b2ddb4c017952f89c
Francis1104OL <taiwofrancis001@gmail.com>
2026-04-01 16:01:09 +0100

    Day_84

---
+++ Day_84-Authorization/Controllers/authController.js
@@ -0,0 +1,104 @@
+const User = require('../Models/userModel')
+const asyncErrorHandler = require('../Utils/asyncErrorHandler')
+const jwt = require('jsonwebtoken')
+const CustomError = require('../Utils/CustomError')
+const util = require ('util')
+
+const signToken = id=>{
+   return jwt.sign({id}, process.env.SECRET_STR,{
+    expiresIn:process.env.LOGIN_EXPIRES
+   })
+}
+ 
+exports.signup= asyncErrorHandler(async (req, res, next)=>{
+   const newUser = await User.create(req.body);
+   
+   const token =  signToken(newUser._id)
+   res.status(201).json({
+    status: 'success',
+    token,
+    data:{
+        user: newUser
+    }
+   });
+})
+//Check if email and password is present in the request body
+exports.login = asyncErrorHandler(async (req, res, next) =>{
+    const email = req.body.email;
+    const password = req.body.password
+    //or using object destructuring syntax
+    // const{email,password} =req.body
+    if(!email || !password){
+    return next(new CustomError('Please provide email & password', 400));
+}
+//check if the user exist with the given email
+const user = await User.findOne({email:email}).select('+password');
+//const isMatch =await comparePasswordInDb(password,user.password)
+
+//check if user exist and the password matches 
+if (!user|| !(await user.comparePasswordInDb(password))){
+    const error = new CustomError('Incorrect email or password', 400)
+    return next(error)
+}
+
+const token =  signToken(user._id)
+    res.status(200).json({
+        status: 'success',
+        token
+      //  user: i don't want to return the user details
+    })
+})
+
+
+
+
+exports.protect = asyncErrorHandler(async(req, res, next)=>{
+//1. Read the token and check if it exist
+const testToken = req.headers.authorization
+let token;
+if (testToken && testToken.startsWith('Bearer')){
+    token = testToken.split(' ')[1];
+
+}
+if (!token){
+    return next(new CustomError('You are not logged in', 401))
+}
+
+
+//2 validate the token
+const decodedToken = await util.promisify(jwt.verify)(token, process.env.SECRET_STR)
+console.log(decodedToken)
+//3. if the user exist
+const user = await User.findById(decodedToken.id)
+if(!user){
+    return next(new CustomError('The user with the given token does not exist', 401))
+}
+
+const isPasswordChanged =await user.isPasswordChanged(decodedToken.iat)
+//4. if the user change the password after the token was issued
+if(isPasswordChanged){
+    return next(new CustomError('The password has been changed recently', 401))
+};
+
+req.user = user;
+next();
+})
+//A WRAPPER FUNCTION
+exports.restrict = (role)=>{
+    return(req, res, next)=>{
+        if(req.user.role !== role){
+            const error = new CustomError('You do not have permission to perform this action', 403)
+        }
+        next();
+    }
+}
+
+// // WHEN THERE'S MORE THAN 1 ROLES to perform an action 
+// exports.restrict = (...role)=>{
+//     return(req, res, next)=>{
+//         if(!req.includes(req.user.role)){
+//             return next(new CustomError('You do not have permission to perform this action', 403))
+//         }
+//         next();
+//     }
+// }
---
+++ Day_84-Authorization/app.js
@@ -0,0 +1,46 @@
+//Import expresss
+const express = require('express');
+const app = express();
+const CustomError = require('./Utils/CustomError')
+const globalErroeHandler = require('./Controllers/errorController')
+const morgan = require('morgan')
+const moviesRouter= require('./Routes/moviesRoutes');
+const authRouter =require('./Routes/authRouter')
+//Route Handler Functions
+
+const logger =function(req, res, next){
+    console.log('custom middleware called')
+    next()
+    
+}
+app.use(logger)
+app.use(express.json())
+app.set('query parser', 'extended');
+if(process.env.NODE_ENV=='development'){
+app.use(morgan('dev'))
+}
+app.use(express.static('./public'))//static file
+
+//app.use(morgan('combined'))
+//app.use(morgan('common'))
+//app.use(morgan('short'))
+//app.use(morgan('tiny'))
+
+app.use((req, res, next)=>{
+    req.requestedAt = new Date().toISOString();
+    next( )
+})
+
+app.use('/api/v1/movies', moviesRouter)// mounting route
+app.use('/api/v1/users', authRouter)
+//Default URL:must be the last in the define route
+app.use((req, res, next) => {
+
+    const err = new CustomError(`Can't find ${req.originalUrl} on the server`, 404)
+    next(err)
+
+});
+app.use(globalErroeHandler)
+module.exports= app;
+
+ 
